cb4238453d29ad002e7d4a4b898ce9dff01283e3
Olim2508 <rahmatovolim3@gmail.com>
2023-05-12 18:44:29 +0500

    added jest tests

--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 run ts files command:
 ```ts-node codewars.ts```
+
+Running tests
+```npm test```
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,4 +5,8 @@ describe('testing digital root function', () => {
   test('test 2 digit number', () => {
     expect(digitalRoot(16)).toBe(7);
   });
+
+  test('test 6 digit number', () => {
+    expect(digitalRoot(132189)).toBe(6);
+  });
 });
